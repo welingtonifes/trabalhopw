f190562ed3793cae546dd0afbb991d415a51c8ff
wel <wel@welmint>
2014-07-27 19:17:56 -0300

    adicionado ob_start na página index pra poder usar session sem aparecer warnnings no lampp,xampp. iniciando crud servicos. ainda nao funcional.

--- app/app/controller/dentista/procRelatorioDentista.php
+++
@@ -1,9 +0,0 @@
-<?php
-     //pega os dados do banco e converte em json pra usar no datatables(jquery)
-    //obs: só funcionou com PDO
-    $pdo = new PDO('mysql:host=localhost;dbname=sgco', 'root', '');
-    $pdo -> exec("SET CHARACTER SET utf8");
-    $listar = $pdo->query('select * from dentista'); 
-    echo json_encode($listar->fetchAll(PDO::FETCH_OBJ));
-   
-?>
